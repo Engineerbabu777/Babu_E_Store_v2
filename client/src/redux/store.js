74821fdaad4e21b8d626d09426fc177784ace523
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-12 19:08:37 +0500

    'added'

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userReducer } from './reducers/user'
+import { productReducer } from "./reducers/product";
+import { cartReducer } from "./reducers/cart";
+import { wishlistReducer } from "./reducers/wishlist";
+import { sellerReducer } from './reducers/seller';
 
 const Store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    products: productReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+    seller:sellerReducer
+
   }
 })
 
